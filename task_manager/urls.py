5fefe5f5a33283e58129f13d311461d9ff1e817c
Lev <lev01111989@gmail.com>
2025-08-22 14:32:51 +0200

    create/dit task_manager/urls.py

---
+++ task_manager/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path
+from . import views
+
+app_name = "task_manager"
+
+urlpatterns = [
+    path(
+        "",
+        views.TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/<int:pk>/",
+        views.TaskDetailView.as_view(),
+        name="task-detail"
+    ),
+    path(
+        "tasks/create/",
+        views.TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/edit/",
+        views.TaskUpdateView.as_view(),
+        name="task-edit"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        views.TaskToggleCompleteView.as_view(),
+        name="task-toggle"
+    ),
+    path(
+        "workers/",
+        views.WorkerListView.as_view(),
+        name="worker-list"
+    ),
+    path(
+        "workers/<int:pk>/",
+        views.WorkerDetailView.as_view(),
+        name="worker-detail"
+    ),
+]
