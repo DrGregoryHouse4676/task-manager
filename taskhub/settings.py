5b4febe12ff0492bc78c1e24072609a9e1399548
Lev <lev01111989@gmail.com>
2025-08-26 14:18:33 +0200

    commit settings and requirements.txt

--- taskhub/settings.py
+++ taskhub/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
-LOGIN_REDIRECT_URL = "task_manager:task-list"
+LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = "login"
 
