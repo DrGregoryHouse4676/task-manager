898aea60f401e2b94882be6f865c0e47221cb95f
Lev <lev01111989@gmail.com>
2025-09-03 09:13:36 +0200

    Fix settings

--- taskhub/settings/base.py
+++ taskhub/settings/base.py
@@ -7,9 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
 DEBUG = False
 
 ALLOWED_HOSTS: list[str] = []
