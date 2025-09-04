ef3f6a096aa726df8ac2ff5d6556643136997e0b
Lev <lev01111989@gmail.com>
2025-09-04 13:38:55 +0200

    edit forms

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -19,8 +19,8 @@ class TaskForm(forms.ModelForm):
             "priority": forms.Select(attrs={"class": "form-select"}),
             "task_type": forms.Select(attrs={"class": "form-select"}),
             "project": forms.Select(attrs={"class": "form-select"}),
-            "assignees": forms.CheckboxSelectMultiple(),
-            "tags": forms.CheckboxSelectMultiple(),
+            "assignees": forms.SelectMultiple(attrs={"class": "form-select", "size": 8}),
+            "tags": forms.SelectMultiple(attrs={"class": "form-select", "size": 8}),
         }
         help_texts = {
             "assignees": "",
